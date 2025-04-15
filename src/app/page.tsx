66bf2953c86703e16e08c823875813916630b63f
PY-Dev20 <aboodi.nour@gmail.com>
2025-04-15 09:27:53 +0400

    Basic Setup , Shadcn ui, Theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h2 className="font-bold text-blue-500 ">Welcome to me</h2>
+   <div className="flex flex-col items-right justify-center min-h-screen p-24">
+    <div className="w-100 flex justify-end">
+      <ThemeToggle />
     </div>
+    <h1 className=" text-blue-500 font-barlow ">Welcome to me</h1>
+    
+    <Button variant="destructive">Submit</Button>
+   
+    </div>
+   
    
   );
 }
